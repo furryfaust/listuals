5d734e6c77b19032ee106a168c57a315815861bb
Kevin <kevtehpro35@gmail.com>
2016-01-12 23:25:55 -0500

    Refactor code

--- assets/js/run.js
+++ assets/js/run.js
@@ -97,51 +97,37 @@ var NODE_WIDTH = 100, NODE_HEIGHT = 50;
 function generate(list, canvas, x, y) {
     var node = list.head;
     while (node != null) {
+        canvas.strokeRect(x, y, NODE_WIDTH, NODE_HEIGHT);
+        canvas.moveTo(x + 50, y);
+        canvas.lineTo(x + 50, y + 50);
+        canvas.stroke();
+
+        if (node.next != null) {
+            canvas.moveTo(x + NODE_WIDTH, y + NODE_HEIGHT/2);
+            canvas.lineTo(x + NODE_WIDTH + 50, y + NODE_HEIGHT/2);
+            canvas.stroke();
+        } else {
+            canvas.moveTo(x + NODE_WIDTH/2, y + NODE_HEIGHT);
+            canvas.lineTo(x + NODE_WIDTH, y);     
+            canvas.stroke();
+        }
+
         switch (node.type) {
             case "node":
-                canvas.strokeRect(x, y, NODE_WIDTH, NODE_HEIGHT);
-                canvas.moveTo(x + 50, y);
-                canvas.lineTo(x + 50, y + 50);
                 canvas.fillText(node.value, x + 25, y + 35);
                 canvas.stroke();
-
-                if (node.next != null) {
-                    canvas.moveTo(x + NODE_WIDTH, y + NODE_HEIGHT/2);
-                    canvas.lineTo(x + NODE_WIDTH + 50, y + NODE_HEIGHT/2);
-                    canvas.stroke();
-                } else {
-                    canvas.moveTo(x + NODE_WIDTH/2, y + NODE_HEIGHT);
-                    canvas.lineTo(x + NODE_WIDTH, y);     
-                    canvas.stroke();
-                }
-
-                x += 150;
                 break;
             case "list":
-                canvas.strokeRect(x, y, NODE_WIDTH, NODE_HEIGHT);
-                canvas.moveTo(x + 50, y);
-                canvas.lineTo(x + 50, y + 50);
-                canvas.stroke();
-
-                if (node.next != null) {
-                    canvas.moveTo(x + NODE_WIDTH, y + NODE_HEIGHT/2);
-                    canvas.lineTo(x + NODE_WIDTH + 50, y + NODE_HEIGHT/2);
-                    canvas.stroke();
-                } else {
-                    canvas.moveTo(x + NODE_WIDTH/2, y + NODE_HEIGHT);
-                    canvas.lineTo(x + NODE_WIDTH, y);     
-                    canvas.stroke();
-                }
-
                 if (node.head != null) {
                     canvas.moveTo(x + 25, y + NODE_HEIGHT);
                     canvas.lineTo(x + 25, offsetY + 100);
                     offsetY += 100;
                     generate(node, canvas, x, offsetY);
                 }
-                x += 150;
                 break;
         }
+
+        x += 150;
         node = node.next;
     }
 }
